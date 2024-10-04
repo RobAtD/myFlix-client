44d115ca39e4d02aba0f3810c19b694ada56840a
RobAtD <r.ditler@devduck.de>
2024-10-04 14:26:46 +0200

    removes hardcoded movies & connects to api

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,44 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            title: 'Star Wars Episode 1: The Phantom Menace',
-            description: 'The beginning of the legendary Sci-Fy saga.',
-            genre: 'Sci-Fy',
-            director: 'George Lucas',
-            actors: ['Ewan McGregor', 'Liam Neeson'],
-            releaseYear: 1999,
-            image: 'https://upload.wikimedia.org/wikipedia/en/4/40/Star_Wars_Phantom_Menace_poster.jpg',
-        },
-        {
-            id: 2,
-            title: 'The Lord of the Rings: The Fellowship of the Ring',
-            description:
-                'A young hobbit, Frodo, embarks on a quest to destroy the One Ring.',
-            genre: 'Fantasy',
-            director: 'Peter Jackson',
-            actors: ['Elijah Wood', 'Ian McKellen', 'Viggo Mortensen'],
-            releaseYear: 2001,
-            image: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Lord_Rings_Fellowship_Ring.jpg',
-        },
-        {
-            id: 3,
-            title: 'Goodfellas',
-            description:
-                'The rise and fall of a mob associate and his associates in the criminal underworld.',
-            genre: 'Thriller',
-            director: 'Martin Scorsese',
-            actors: ['Robert De Niro', 'Ray Liotta'],
-            releaseYear: 1990,
-            image: 'https://upload.wikimedia.org/wikipedia/en/7/7b/Goodfellas.jpg',
-        },
-    ]);
+    const [movies, setMovies] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect(() => {
+        fetch('https://robs-movie-api-981dce4af120.herokuapp.com/movies')
+            .then((response) => response.json())
+            .then((data) => {
+                const moviesFromApi = data.map((doc) => {
+                    return {
+                        id: doc._id,
+                        title: doc.Title,
+                        description: doc.Description,
+                        genre: doc.Genre,
+                        director: doc.Director,
+                        actors: doc.Actors,
+                        image: doc.ImageURL,
+                        releaseYear: doc.ReleaseYear,
+                        featured: doc.Featured,
+                    };
+                });
+                setMovies(moviesFromApi);
+            });
+    }, []);
 
     if (selectedMovie) {
         return (
